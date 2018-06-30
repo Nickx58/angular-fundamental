61b07912b1a0ddcfdd413c34cc6c11e47e3cf02b
abhishek <abhishek@heymojo.com>
2018-06-30 12:36:29 +0530

    Project setup

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	styleUrls: ['app.component.scss'],
+	template: `
+		<div class="app">
+			Hello!
+			<button> Hwllo</button>
+		</div>
+	`
+})
+export class AppComponent {
+
+}
