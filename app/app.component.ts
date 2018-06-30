6b97c2ff5f0cf216658431d18162008ce9192ed7
abhishek <nikhilx58@gmail.com>
2018-06-30 22:25:44 +0530

    class,ngClass,style and ngStyle binding in Angular to apply css class dynamically

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,8 +11,31 @@ interface Passenger {
 	styleUrls: ['./app.component.scss'],
 	template: `
 		<div>
+			<h2> class Binding </h2>
 			<ul>
 				<li *ngFor="let passenger of passengers; let i = index;">
+					<span class="status" [class.checked-in]="passenger.checkedin"> </span>
+					{{ i+1 }} {{ passenger.fullname }}
+				</li>
+			</ul>
+			<h2> ngClass Binding </h2>
+			<ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<span class="status" [ngClass]="{'checked-in':passenger.checkedin}"> </span>
+					{{ i+1 }} {{ passenger.fullname }}
+				</li>
+			</ul>
+			<h2> style binding </h2>
+			<ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<span class="status" [style.backgroundColor]="(passenger.checkedin ? '#2ecc71' : '#c0392b')"> </span>
+					{{ i+1 }} {{ passenger.fullname }}
+				</li>
+			</ul>
+			<h2> ngStyle Binding </h2>
+			<ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<span class="status" [ngStyle]="{backgroundColor:(passenger.checkedin ? '#2ecc71' : '#c0392b')}"> </span>
 					{{ i+1 }} {{ passenger.fullname }}
 				</li>
 			</ul>
@@ -32,7 +55,7 @@ export class AppComponent {
 	},{
 		id: 3,
 		fullname: 'Tushar',
-		checkedin: false
+		checkedin: true
 	},{
 		id: 4,
 		fullname: 'Raman',
@@ -40,6 +63,6 @@ export class AppComponent {
 	},{
 		id: 5,
 		fullname: 'Hemu',
-		checkedin: false
+		checkedin: true
 	}]
 }
