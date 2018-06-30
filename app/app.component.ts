0683e394073d5aed46c08e941c21edeee082dc44
abhishek <abhishek@heymojo.com>
2018-06-30 18:49:23 +0530

    Example of property and event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	styleUrls: ['app.component.scss'],
+	styleUrls: ['./app.component.scss'],
 	template: `
-		<div class="app">
-			Hello!
-			<button> Hwllo</button>
+		<div>
+			<button (click)="handleName()"> Change Name </button>
+			<input
+				(input)="handleInput($event)" 
+				type="text" 
+				[value]="name" />
+			{{ name }}
 		</div>
 	`
 })
-export class AppComponent {
 
+export class AppComponent {
+	// property binding
+	name: string = 'Nikhil'
+
+	constructor() { }
+	 handleInput(event: any) {
+		this.name = event.target.value;
+	}
+	handleName() {
+		this.name = "Manu"
+	}
 }
