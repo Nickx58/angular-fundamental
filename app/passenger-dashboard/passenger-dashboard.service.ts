c707e43cdb1e78f9da4833c4b89c10c17b62e3f9
Nickx58 <nikhilx58@gmail.com>
2018-07-06 09:30:19 +0530

    Remove, Edit request with HTTP

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Passenger } from './models/passenger';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -12,6 +12,26 @@ export class PassengerDashboardService {
 	getPassengers(): Observable<Passenger[]> {
 		return this.http
 			.get(PASSENGER_API)
+			.map((response: Response) => response.json());
+	}
+
+	updatePassengers(passenger: Passenger): Observable<Passenger> {
+		let headers = new Headers({
+			'Content-Type': 'application/json'
+		});
+
+		let options = new RequestOptions({
+			headers: headers
+		});
+
+		return this.http
+			.put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+			.map((response: Response) => response.json());
+	}
+
+	removePassengers(passenger: Passenger): Observable<Passenger> {
+		return this.http
+			.delete(`${PASSENGER_API}/${passenger.id}`)
 			.map((response: Response) => response.json());
 	}
 }
