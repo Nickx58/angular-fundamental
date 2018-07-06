3d455b1ec69a39df95ce75b72d49a206cc149d74
Nickx58 <nikhilx58@gmail.com>
2018-07-06 17:28:08 +0530

    Error handling with Observable

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 const PASSENGER_API: string = '/api/passengers'
 
@@ -12,7 +14,8 @@ export class PassengerDashboardService {
 	getPassengers(): Observable<Passenger[]> {
 		return this.http
 			.get(PASSENGER_API)
-			.map((response: Response) => response.json());
+			.map((response: Response) => response.json())
+			.catch((error: any) => Observable.throw(error.json()));
 	}
 
 	updatePassengers(passenger: Passenger): Observable<Passenger> {
@@ -26,12 +29,14 @@ export class PassengerDashboardService {
 
 		return this.http
 			.put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
-			.map((response: Response) => response.json());
+			.map((response: Response) => response.json())
+			.catch((error: any) => Observable.throw(error.json()));
 	}
 
 	removePassengers(passenger: Passenger): Observable<Passenger> {
 		return this.http
 			.delete(`${PASSENGER_API}/${passenger.id}`)
-			.map((response: Response) => response.json());
+			.map((response: Response) => response.json())
+			.catch((error: any) => Observable.throw(error.json()));
 	}
 }
