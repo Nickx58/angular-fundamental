24b7377d190f04797532041d4a91e63e695f62fe
Nickx58 <nikhilx58@gmail.com>
2018-07-08 16:57:23 +0530

    select option rendering Nd ngValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Passenger } from '../../models/passenger';
+import { Baggage } from '../../models/baggage';
 
 @Component({
 	selector: 'passenger-form',
@@ -10,6 +11,20 @@ import { Passenger } from '../../models/passenger';
 export class PassengerFormComponent {
 	@Input()
 	detail: Passenger;
+
+	baggage: Baggage[] = [{
+		key: 'none',
+		value: 'No baggage'
+	},{
+		key: 'hand-only',
+		value: 'Hand baggage'
+	},{
+		key: 'hold-only',
+		value: 'Hold baggage'
+	},{
+		key: 'hand-hold',
+		value: 'Hand and Hold baggage'
+	}];
 
 	toggleCheckIn(checkedIn: boolean) {
 		if (checkedIn) {
