0ef2884649ce659e73896825f5105de401163d0e
Nickx58 <nikhilx58@gmail.com>
2018-07-08 19:45:40 +0530

    ngSubmit and stateless Output

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Passenger } from '../../models/passenger';
 import { Baggage } from '../../models/baggage';
 
@@ -9,6 +9,10 @@ import { Baggage } from '../../models/baggage';
 })
 
 export class PassengerFormComponent {
+
+	@Output() 
+	update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
 	@Input()
 	detail: Passenger;
 
@@ -29,6 +33,12 @@ export class PassengerFormComponent {
 	toggleCheckIn(checkedIn: boolean) {
 		if (checkedIn) {
 			this.detail.checkedInDate = Date.now();
+		}
+	}
+	
+	handleSubmit(passenger: Passenger, isValid: boolean) {
+		if (isValid) {
+			this.update.emit(passenger);
 		}
 	}
 }
