920a12cd41f249b5ed0ad28b3b6243f2936a6682
Nickx58 <nikhilx58@gmail.com>
2018-07-08 19:58:42 +0530

    Route module routes and router-outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+	{ path: '', component: HomeComponent, pathMatch: 'full'}
+]
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    PassengerDashboardModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+	imports: [
+		BrowserModule,
+		FormsModule,
+		RouterModule.forRoot(routes),
+		PassengerDashboardModule
+	],
+	bootstrap: [
+		AppComponent
+	],
+	declarations: [
+		AppComponent,
+		HomeComponent
+	]
 })
 export class AppModule {}
