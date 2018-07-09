02feb7864e5e0f0ec50f840b8e806169a148d0b5
Nickx58 <nikhilx58@gmail.com>
2018-07-09 21:32:05 +0530

    Adding multiple validations

--- app/courses/courses.component.ts
+++ app/courses/courses.component.ts
@@ -4,24 +4,7 @@ import { CoursesService } from './courses.service';
 @Component({
 	selector: 'app-mosh',
 	styleUrls: ['./courses.component.scss'],
-	template: `
-		<div *ngIf="courses.length > 0; then course else noCourse"></div>
-		<template #course>
-			<h1> Courses as Follow </h1>
-		</template>
-		<template #noCourse>
-		<h1> No course try later </h1>
-		</template>
-		<ul>
-			<li *ngFor="let course of courses">{{ course }}</li>
-		</ul>
-		<div>
-			<input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
-		</div>
-		<div>
-			<hm-button [isClicked]="isClicked" (update)="onUpdate()"></hm-button>
-		</div>
-	`
+	templateUrl: './courses.component.html'
 })
 
 export class CoursesComponent {
