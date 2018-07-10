52f14d5824e375373eb3f3eb8b05dbcaab086d3f
Nickx58 <nikhilx58@gmail.com>
2018-07-10 16:56:38 +0530

    FormGroup,FormControl and binding template form

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
-    selector: 'stock-inventory',
-    styleUrls: ['./stock-inventory.component.scss'],
-    template: `Hello`
+	selector: 'stock-inventory',
+	styleUrls: ['./stock-inventory.component.scss'],
+	template: `
+		<form [formGroup]="form" (ngSubmit)="onSubmit()">
+			<div formGroupName="store">
+				<input formControlName="branch" type="text" placeholder="Branch Code" />
+				<input formControlName="code" type="text" placeholder="Manager Code" />
+			</div>
+			<div class="stock-inventory__buttons">
+				<button type="submit" [disabled]="form.invalid"> Order Stock </button>
+			</div>
+			<pre> {{ form.value | json }} </pre>
+		</form>
+	`
 })
 
-export class StockInventoryComponent {}
+export class StockInventoryComponent {
+	form = new FormGroup({
+		store: new FormGroup({
+			branch: new FormControl('B1B2'),
+			code: new FormControl('1234')
+		})
+	})
+
+	onSubmit() {
+		console.log(this.form.value);
+	}
+}
