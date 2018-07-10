c64324fa8af2d898b216c3cbfc8e726ac9059b84
Nickx58 <nikhilx58@gmail.com>
2018-07-10 07:55:06 +0530

    Reactive form, adding validatons

---
+++ app/courses/components/reactive-form/reactive-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+	selector: 'reactive-form',
+	styleUrls: ['./reactive-form.component.scss'],
+	templateUrl: './reactive-form.component.html'
+})
+
+export class ReactiveFormComponent {
+	form = new FormGroup({
+		username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+		password: new FormControl('', Validators.required)
+	});
+
+	get username() {
+		return this.form.get('username');
+	}
+
+	get password() {
+		return this.form.get('password');
+	}
+
+}
